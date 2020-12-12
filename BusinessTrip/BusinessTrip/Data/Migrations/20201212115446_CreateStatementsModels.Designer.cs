8f01f586d7f6f82f181549b6b5087c78de6e00ec
MykolaPMP <mkrainyk26@gmail.com>
2020-12-12 14:50:36 +0200

    Adding Statements Models

--- BusinessTrip/BusinessTrip/Data/Migrations/20201212115446_CreateStatementsModels.Designer.cs
+++ BusinessTrip/BusinessTrip/Data/Migrations/20201212115446_CreateStatementsModels.Designer.cs
@@ -4,14 +4,16 @@ using BusinessTrip.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BusinessTrip.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201212115446_CreateStatementsModels")]
+    partial class CreateStatementsModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
